0bdb72cec4fe86496c7aaf075301b121cdd3330c
Jim Ulrich <jupagblkxten@gmail.com>
2017-03-30 17:27:43 -0500

    added verification rules

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import webapp2
 import cgi
+import re
 
 #def build_form():
 
@@ -12,7 +13,7 @@ class MainHandler(webapp2.RequestHandler):
             <table><tbody>
             <tr><td>
             <label>Username</label></td>
-            <td><input name = 'username' type='text' value required>
+            <td><input name='username' type='text' value required>
             </td></tr>
 
             <tr><td>
@@ -34,8 +35,39 @@ class MainHandler(webapp2.RequestHandler):
             <input type='submit'>
         </form>
             """
+    #def write_form(self)
 
         self.response.write(header + form)
+
+    def post(self):
+
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+        def valid_username(username):
+            return USER_RE.match(username)
+
+        PASS_RE = re.compile(r"^.{3,20}$")
+        def valid_password(password):
+            return PASS_RE.match(password)
+
+        EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+        def valid_email(email):
+            return EMAIL_RE.match(email)
+
+        x = "valid"
+
+        if not valid_username(username):
+            x = "not vaild"
+
+        self.response.write(x)
+
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
